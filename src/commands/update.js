aee54075a94b6597f89a4b98bc267e1327dd433b
itzJOHv <me@djoh.ml>
2021-09-23 00:51:58 +0300

    Update update.js

--- src/commands/update.js
+++ src/commands/update.js
@@ -8,9 +8,9 @@ const { exec } = require("child_process");
 
 module.exports.run = async (message) => {
     exec("git stash push --include-untracked");
-    exec("git pull origin main", (error, stdout) => {
+    exec("git pull", (error, stdout) => {
         exec("git stash drop");
-        if (error) return message.reply(error, { code: "fix" });
+        if (error) return message.reply(`\`\`\`fix\n${error}\n\`\`\``);
 
         if (stdout.includes("Already up to date.")) {
             message.reply("Bot already up to date. No changes since last pull.");
