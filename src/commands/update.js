6d58a1026079be8237c9908251c8178437345207
itzJOHv <me@djoh.ml>
2021-09-23 00:55:19 +0300

    Update update.js

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,21 +1,21 @@
 module.exports = {
-    aliases: ["upd", "u"],
     permissionRequired: 5,
-    checkArgs: (args) => !args.length
+    slash: true
 };
 
 const { exec } = require("child_process");
+const { Interaction } = require("discord.js");
 
-module.exports.run = async (message) => {
+module.exports.run = async (interaction = new Interaction) => {
     exec("git stash push --include-untracked");
     exec("git pull", (error, stdout) => {
         exec("git stash drop");
-        if (error) return message.reply(`\`\`\`fix\n${error}\n\`\`\``);
+        if (error) return interaction.reply(`\`\`\`fix\n${error}\n\`\`\``);
 
         if (stdout.includes("Already up to date.")) {
-            message.reply("Bot already up to date. No changes since last pull.");
+            interaction.reply("Bot already up to date. No changes since last pull.");
         } else {
-            message.reply("Pulled from GitHub. Restarting the bot.\n\nLogs:\n```\n" + res + "\n```")
+            interaction.reply("Pulled from GitHub. Restarting the bot.\n\nLogs:\n```\n" + res + "\n```")
                 .then(() => setTimeout(() => process.exit(), 1000));
         };
     });
