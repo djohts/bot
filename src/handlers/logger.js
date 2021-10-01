0fd6967d4b69500644bece4ac9c39c9add8efa82
itzJOHv <me@djoh.ml>
2021-10-02 01:40:42 +0300

    Update logger.js

--- src/handlers/logger.js
+++ src/handlers/logger.js
@@ -4,26 +4,26 @@ let channel = require("../bot").client.channels.cache.get(logChannel);
 module.exports = {
     log: (output, embedData = {}) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
-        if (embedData && channel) channel.send({
+        console.log(chalk.whiteBright(`[${timeFormatted} - INFO]`, output));
+        if (embedData.description && channel) channel.send({
             content: `\`[${timeFormatted} - INFO]\``,
             embeds: [embedData]
         });
-        console.log(chalk.whiteBright(`[${timeFormatted} - INFO]`, output));
     },
     warn: (output, embedData = {}) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
-        if (embedData && channel) channel.send({
+        console.log(chalk.yellowBright(`[${timeFormatted} - WARN]`, output));
+        if (embedData.description && channel) channel.send({
             content: `\`[${timeFormatted} - WARN]\``,
             embeds: [embedData]
         });
-        console.log(chalk.yellowBright(`[${timeFormatted} - WARN]`, output));
     },
     error: (output, embedData = {}) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
-        if (embedData && channel) channel.send({
+        console.log(chalk.redBright(`[${timeFormatted} - ERROR]`, output));
+        if (embedData.description && channel) channel.send({
             content: `<@419892040726347776> \`[${timeFormatted} - ERROR]\``,
             embeds: [embedData]
         });
-        console.log(chalk.redBright(`[${timeFormatted} - ERROR]`, output));
     }
 };
