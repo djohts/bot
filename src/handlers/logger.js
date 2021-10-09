3dcae165abda5f3a979981d040b93578974654ed
itzJOHv <me@djoh.ml>
2021-10-09 22:01:44 +0300

    Update logger.js

--- src/handlers/logger.js
+++ src/handlers/logger.js
@@ -4,7 +4,7 @@ const log = new WebhookClient({ url: log_url });
 module.exports = {
     log: async (output, embedData = new MessageEmbed) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
-        if (output.length) console.log(chalk.whiteBright(`[${timeFormatted} - INFO]`, output));
+        if (output) console.log(chalk.whiteBright(`[${timeFormatted} - INFO]`, output));
 
         if (embedData.description && log) return await log.send({
             content: `\`[${timeFormatted} - INFO]\``,
@@ -13,7 +13,7 @@ module.exports = {
     },
     warn: async (output, embedData = new MessageEmbed) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
-        if (output.length) console.log(chalk.yellowBright(`[${timeFormatted} - WARN]`, output));
+        if (output) console.log(chalk.yellowBright(`[${timeFormatted} - WARN]`, output));
 
         if (embedData.description && log) await log.send({
             content: `\`[${timeFormatted} - WARN]\``,
@@ -22,7 +22,7 @@ module.exports = {
     },
     error: async (output, embedData = new MessageEmbed) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
-        if (output.length) console.log(chalk.redBright(`[${timeFormatted} - ERROR]`, output));
+        if (output) console.log(chalk.redBright(`[${timeFormatted} - ERROR]`, output));
 
         if (embedData.description && log) return await log.send({
             content: `<@419892040726347776> \`[${timeFormatted} - ERROR]\``,
