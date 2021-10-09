ae393f8821a1d33336aa3dbd7b9aa8c3eedfb9d6
itzJOHv <me@djoh.ml>
2021-10-09 22:00:20 +0300

    Update eval.js

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -12,13 +12,7 @@ module.exports.run = async (message = new Message, args) => {
 	let content = args.join(" ");
 
 	try {
-		let evaled = await eval(content).catch(e => {
-			let err;
-			if (typeof e == "string") err = e.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-			else err = e;
-
-			message.reply(`\`\`\`js\n${err}\n\`\`\``).then(m => setTimeout(() => deleteMessage(m), 30 * 1000)).catch();
-		});
+		let evaled = await eval(content);
 		if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
 
 		if (evaled.length > 2000) message.react("✅").catch();
