f02b212b33400c16f1f751fabd4e98b9e2e1c20e
itzJOHv <me@djoh.ml>
2021-10-10 04:32:13 +0300

    Update logger.js

--- src/handlers/logger.js
+++ src/handlers/logger.js
@@ -6,28 +6,28 @@ module.exports = {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
         if (output) console.log(chalk.whiteBright(`[${timeFormatted} - INFO]`, output));
 
-        if (embedData.description && log) return await log.send({
+        return await log.send({
             content: `\`[${timeFormatted} - INFO]\``,
             embeds: [embedData]
-        });
+        }).catch(() => { });
     },
     warn: async (output, embedData = new MessageEmbed) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
         if (output) console.log(chalk.yellowBright(`[${timeFormatted} - WARN]`, output));
 
-        if (embedData.description && log) await log.send({
+        return await log.send({
             content: `\`[${timeFormatted} - WARN]\``,
             embeds: [embedData]
-        });
+        }).catch(() => { });
     },
     error: async (output, embedData = new MessageEmbed) => {
         const timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
         if (output) console.log(chalk.redBright(`[${timeFormatted} - ERROR]`, output));
 
-        if (embedData.description && log) return await log.send({
+        return await log.send({
             content: `<@419892040726347776> \`[${timeFormatted} - ERROR]\``,
             embeds: [embedData]
-        });
+        }).catch(() => { });
     },
     edit: async (message, data) => {
         return await log.editMessage(message, data);
