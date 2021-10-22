f13022b2026080157953acdac46876c4005aee1d
itzJOHv <me@djoh.ml>
2021-10-23 02:54:13 +0300

    Update buttons.js

--- src/handlers/interactions/buttons.js
+++ src/handlers/interactions/buttons.js
@@ -2,18 +2,15 @@ const { ButtonInteraction } = require("discord.js");
 const { deleteMessage } = require("../../handlers/utils");
 
 module.exports = async (interaction = new ButtonInteraction) => {
-    if (interaction.customId == "reply:delete") {
-        if (["DEFAULT", "REPLY"].includes(interaction.message.type)) {
-            if (
-                interaction.user.id != (await interaction.channel.messages.fetch(interaction.message.reference.messageId).then((m) => m.author.id))
-            ) return interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
-            return deleteMessage(interaction.message);
-        };
-        if (interaction.message.type == "APPLICATION_COMMAND") {
-            if (
-                interaction.user.id != interaction.message.interaction.user.id
-            ) return interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
-            return deleteMessage(interaction.message);
-        };
+    if (["DEFAULT", "REPLY"].includes(interaction.message.type)) {
+        if (
+            interaction.user.id != (await interaction.channel.messages.fetch(interaction.message.reference.messageId).then((m) => m.author.id))
+        ) return interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
+    } else if (interaction.message.type == "APPLICATION_COMMAND") {
+        if (
+            interaction.user.id != interaction.message.interaction.user.id
+        ) return interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
     };
+
+    if (interaction.customId == "reply:delete") return deleteMessage(interaction.message);
 };
