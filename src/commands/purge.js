fa491adb1a42f3fac4088bd755d7406b7ce4dbe2
itzJOHv <me@djoh.ml>
2021-11-02 21:28:00 +0200

    Update purge.js

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -25,9 +25,9 @@ const { CommandInteraction } = require("discord.js");
 const db = require("../database/")();
 
 module.exports.run = async (interaction = new CommandInteraction) => {
-    if (cooldowns.has(interaction.guild.id))
+    if (cooldowns.has(interaction.channel.id))
         return interaction.reply({ content: "❌ Подождите несколько секунд перед повторным использованем команды.", ephemeral: true });
-    else cooldowns.add(interaction.guild.id) && setTimeout(() => cooldowns.delete(interaction.guild.id), 2000);
+    else cooldowns.add(interaction.channel.id) && setTimeout(() => cooldowns.delete(interaction.channel.id), 4000);
 
     const guilddb = await db.guild(interaction.guild.id);
 
