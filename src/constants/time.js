03150b67a779d1ebb84792393fe3fd85f3a11512
itzJOHv <me@djoh.ml>
2021-11-12 19:15:00 +0200

    Update time.js

--- src/constants/time.js
+++ src/constants/time.js
@@ -16,3 +16,10 @@ module.exports.msToTime = (ms) => {
 
     return str?.trim() || "0с";
 };
+
+module.exports.getDateFormatted = (d) => {
+    let month = (d.getMonth() + 1).toString(), day = d.getDate().toString(), year = d.getFullYear();
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+    return [year, month, day].join("-");
+};
