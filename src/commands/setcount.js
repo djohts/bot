ec7a2e8885b7da7a5bdacf75feab7bfa0485d778
itzJOHv <me@djoh.ml>
2021-11-23 17:38:31 +0200

    Create setcount.js

---
+++ src/commands/setcount.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: "setcount",
+    description: "Сменить текущий счёт.",
+    permissionRequired: 1,
+    opts: [{
+        name: "count",
+        description: "Новый счёт.",
+        type: 4,
+        min_value: 0,
+        required: true
+    }],
+    slash: true
+};
+
+const db = require("../database/")();
+const { CommandInteraction } = require("discord.js");
+
+module.exports.run = async (interaction = new CommandInteraction) => {
+    const gdb = await db.guild(interaction.guild.id);
+    const count = interaction.options.getInteger("count");
+
+    gdb.set("count", count);
+
+    return interaction.reply({
+        content: `✅ Новый текущий счёт - **\`${count}\`**.`,
+        ephemeral: (gdb.get().channel == interaction.channel.id)
+    });
+};
