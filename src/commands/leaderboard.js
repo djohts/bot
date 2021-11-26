aa0d56939ab06a61a2e5d7c4ec402c2f6103d72a
itzJOHv <me@djoh.ml>
2021-11-26 19:53:02 +0200

    Create leaderboard.js

---
+++ src/commands/leaderboard.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: "leaderboard",
+    description: "Список лидеров счёта.",
+    permissionRequired: 0,
+    opts: [],
+    slash: true
+};
+
+const db = require("../database/")();
+const { CommandInteraction } = require("discord.js");
+const { formatScore } = require("../constants/");
+
+module.exports.run = async (interaction = new CommandInteraction) => {
+    const gdb = await db.guild(interaction.guild.id);
+    const { users, channel } = gdb.get();
+    const sorted = Object.keys(users).sort((a, b) => users[b] - users[a]);
+    const top = sorted.slice(0, 25);
+    const leaderboard = top.map((id, index) => formatScore(id, index, users, interaction.user.id));
+    let description = leaderboard.join("\n");
+    if (!top.includes(interaction.user.id)) {
+        if (leaderboard.length) description = description + "\n^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+        description = description + formatScore(interaction.user.id, sorted.indexOf(interaction.user.id), users);
+    };
+
+    return interaction.reply({
+        embeds: [{
+            title: `Таблица лидеров ${interaction.guild.name}`,
+            description
+        }],
+        ephemeral: (channel == interaction.channel.id)
+    });
+};
