e808b090acc09fb53195d443f3cd1a3aba231789
itzJOHv <me@djoh.ml>
2021-11-26 18:21:44 +0200

    Update update.js

--- src/commands/update.js
+++ src/commands/update.js
@@ -15,12 +15,36 @@ module.exports.run = async (interaction = new CommandInteraction) => {
         if (error) return interaction.reply(`\`\`\`fix\n${error}\n\`\`\``);
 
         if (stdout.includes("Already up to date.")) {
-            interaction.reply("Bot already up to date. No changes since last pull.");
+            interaction.reply({
+                content: "Bot already up to date. No changes since last pull.",
+                components: [{
+                    type: 1,
+                    components: [{
+                        type: 2,
+                        emoji: {
+                            name: "🗑"
+                        },
+                        style: 4,
+                        custom_id: "reply:delete"
+                    }]
+                }]
+            });
         } else {
             console.log("Pulled from GitHub. Rebooting all shards.\n\n" + stdout);
             interaction.reply({
                 content: "Pulled from GitHub. Rebooting all shards.\n\nLogs:\n```\n" + stdout + "\n```",
-                fetchReply: true
+                fetchReply: true,
+                components: [{
+                    type: 1,
+                    components: [{
+                        type: 2,
+                        emoji: {
+                            name: "🗑"
+                        },
+                        style: 4,
+                        custom_id: "reply:delete"
+                    }]
+                }]
             }).then(async (i) => {
                 i.react("♻️").then(() => interaction.client.shard.broadcastEval(() => process.exit()));
             });
