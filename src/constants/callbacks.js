ee3283576938b873fdf135aa3106fb695dcc2d05
itzJOHv <me@djoh.ml>
2021-12-05 21:35:14 +0200

    d

--- src/constants/callbacks.js
+++ src/constants/callbacks.js
@@ -2,6 +2,9 @@ const { GuildMember, VoiceChannel } = require("discord.js");
 const db = require("../database/")();
 
 module.exports.voicesJoin = async (member = new GuildMember, channel = new VoiceChannel) => {
+    if (!(member instanceof GuildMember)) return;
+    if (!(channel instanceof VoiceChannel)) return;
+
     const gset = await db.settings(member.guild.id);
     if (!gset.get().voices.enabled) return;
     if (gset.get().voices.lobby != channel.id) return;
@@ -9,13 +12,20 @@ module.exports.voicesJoin = async (member = new GuildMember, channel = new Voice
 
     member.guild.channels.create("Комната " + member.user.tag, {
         type: "GUILD_VOICE",
-        parent: channel.parentId
+        parent: channel.parentId,
+        permissionOverwrites: [{
+            id: member.user.id,
+            allow: ["MANAGE_CHANNELS", "PRIORITY_SPEAKER", "STREAM"]
+        }]
     }).then((ch) => {
         member.voice.setChannel(ch.id) && guilddb.setOnObject("voices", ch.id, member.user.id);
     }).catch(() => { });
 };
 
 module.exports.voicesLeave = async (member = new GuildMember, channel = new VoiceChannel) => {
+    if (!(member instanceof GuildMember)) return;
+    if (!(channel instanceof VoiceChannel)) return;
+
     const guilddb = await db.guild(member.guild.id);
 
     if (guilddb.get().voices[channel.id] == member.user.id) {
@@ -24,6 +34,10 @@ module.exports.voicesLeave = async (member = new GuildMember, channel = new Voic
 };
 
 module.exports.voicesSwitch = async (member = new GuildMember, oldChannel = new VoiceChannel, newChannel = new VoiceChannel) => {
+    if (!(member instanceof GuildMember)) return;
+    if (!(oldChannel instanceof VoiceChannel)) return;
+    if (!(newChannel instanceof VoiceChannel)) return;
+
     const gset = await db.settings(member.guild.id);
     const guilddb = await db.guild(member.guild.id);
 
@@ -33,7 +47,11 @@ module.exports.voicesSwitch = async (member = new GuildMember, oldChannel = new 
     if (gset.get().voices.lobby == newChannel.id && gset.get().voices.enabled) {
         member.guild.channels.create("Комната " + member.user.tag, {
             type: "GUILD_VOICE",
-            parent: newChannel.parentId
+            parent: newChannel.parentId,
+            permissionOverwrites: [{
+                id: member.user.id,
+                allow: ["MANAGE_CHANNELS", "PRIORITY_SPEAKER", "STREAM"]
+            }]
         }).then((ch) => {
             member.voice.setChannel(ch.id) && guilddb.setOnObject("voices", ch.id, member.user.id);
         }).catch(() => { });
