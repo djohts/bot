4a331f3df0e09afb40bc2f1ee01bda61a215e2de
itzJOHv <me@djoh.ml>
2021-12-17 13:05:49 +0200

    Create pause.js

---
+++ src/commands/pause.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: "pause",
+    description: "Поставить плеер на паузу?",
+    permissionRequired: 0,
+    opts: [],
+    slash: true
+};
+
+const { CommandInteraction } = require("discord.js");
+const db = require("../database/")();
+
+module.exports.run = async (interaction) => {
+    if (!(interaction instanceof CommandInteraction)) return;
+    const client = interaction.client;
+    const gdb = await db.guild(interaction.guild.id);
+    if (gdb.get().channel == interaction.channelId) return interaction.reply({ content: "❌ Эта команда недоступна в данном канале.", ephemeral: true });
+
+    if (!interaction.member.voice.channel) return interaction.reply({ content: "❌ Вы должны находится в голосовом канале.", ephemeral: true });
+    if (
+        interaction.guild.me.voice.channel &&
+        interaction.member.voice.channel.id != interaction.guild.me.voice.channel.id
+    ) return interaction.reply({ content: "❌ Вы должны находится в том же голосовом канале, что и я.", ephemeral: true });
+    await interaction.deferReply();
+
+    const player = client.manager.create({
+        guild: interaction.guildId
+    });
+
+    if (player.queue.totalSize) {
+        player.paused ? (() => {
+            player.pause(false);
+            interaction.editReply("Пауза выключена.");
+        })() : (() => {
+            player.pause(true);
+            interaction.editReply("Пауза включёна.");
+        })();
+    } else interaction.editReply("❌ Плеер на паузе или не играет.") && player.destroy();
+};
