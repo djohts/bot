13ecbcf2c65631506eab05e82b3da09080344084
itzJOHv <me@djoh.ml>
2021-12-17 20:20:47 +0200

    Update utils.js

--- src/handlers/utils.js
+++ src/handlers/utils.js
@@ -2,19 +2,19 @@ const bulks = new Map(), rates = new Map();
 const { Message } = require("discord.js");
 
 module.exports.deleteMessage = (message = new Message) => {
-    const rate = rates.get(message.channel.id) || 0;
-    rates.set(message.channel.id, rate + 1);
+    const rate = rates.get(message.channel?.id) || 0;
+    rates.set(message.channel?.id, rate + 1);
 
-    setTimeout(() => rates.set(message.channel.id, rates.get(message.channel.id) - 1), 10000);
+    setTimeout(() => rates.set(message.channel?.id, rates.get(message.channel?.id) - 1), 10000);
 
-    const bulk = bulks.get(message.channel.id) || [];
+    const bulk = bulks.get(message.channel?.id) || [];
     if (bulk.length) bulk.push(message);
     else if (rate < 3) message.delete().catch(() => { });
     else {
-        bulks.set(message.channel.id, [message]);
+        bulks.set(message.channel?.id, [message]);
         setTimeout(() => {
-            message.channel.bulkDelete(bulks.get(message.channel.id)).catch(() => { });
-            bulks.delete(message.channel.id);
+            message.channel.bulkDelete(bulks.get(message.channel?.id)).catch(() => { });
+            bulks.delete(message.channel?.id);
         }, 5000);
     };
 };
