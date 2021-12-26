1126024cba58ebb2f213d4b03af60f0b8f0ada60
itzJOHv <me@djoh.ml>
2021-12-26 22:44:38 +0200

    Update tickers.js

--- src/handlers/tickers.js
+++ src/handlers/tickers.js
@@ -18,7 +18,7 @@ module.exports = async (client = new Client) => {
 
 async function updatePresence(client = new Client) {
     const gc = await client.shard.broadcastEval((bot) => bot.guilds.cache.size).then((res) => res.reduce((prev, curr) => prev + curr, 0));
-    let text = `intents | ${gc} guilds`;
+    let text = `150? -> | ${gc} guilds`;
     return client.user.setPresence({
         status: "idle",
         activities: [{ type: "PLAYING", name: text }],
