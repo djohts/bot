a214a0c072acb3493a02ef8eef6859f76bcfb83e
itzJOHv <me@djoh.xyz>
2022-01-17 01:56:06 +0200

    Update buttons.js

--- src/handlers/interactions/buttons.js
+++ src/handlers/interactions/buttons.js
@@ -3,14 +3,16 @@ const { deleteMessage } = require("../../handlers/utils");
 
 module.exports = async (interaction) => {
     if (!(interaction instanceof ButtonInteraction)) return;
+
     if (interaction.message.type == "REPLY") {
-        if (
-            interaction.user.id != (await interaction.channel.messages.fetch(interaction.message.reference.messageId).then((m) => m.author.id))
-        ) return interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
+        if (interaction.user.id != (await interaction.channel.messages.fetch(interaction.message.reference.messageId).then((m) => m.author.id))) {
+            return await interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
+        };
     } else if (interaction.message.type == "APPLICATION_COMMAND") {
-        if (
-            interaction.user.id != interaction.message.interaction.user.id
-        ) return interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
+        if (interaction.user.id != interaction.message.interaction.user.id) {
+            return await interaction.reply({ content: "❌ Вы не можете использовать это.", ephemeral: true });
+        };
     };
+
     if (interaction.customId == "reply:delete") return deleteMessage(interaction.message);
 };
