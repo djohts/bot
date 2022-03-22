554fcc6a88f3bc13f5cfe3630516299a4b3dbc7c
itzJOHv <me@djoh.xyz>
2022-03-22 11:40:10 +0200

    forgot to build

--- build/src/handlers/lava.js
+++ build/src/handlers/lava.js
@@ -13,15 +13,24 @@ module.exports = (client) => new erela_js_1.Manager({
         client.guilds.cache.get(id)?.shard.send(payload);
     }
 })
-    .on("trackError", console.log)
-    .on("trackStuck", console.log)
+    .on("trackError", ({ textChannel }, { title }, error) => {
+    const text = client.channels.cache.get(textChannel);
+    text?.send(`An error occured when trying to play \`${title}\`: ${error.exception?.cause || error.error}`).catch(() => null);
+})
+    .on("trackStuck", ({ textChannel }, { title }, error) => {
+    const text = client.channels.cache.get(textChannel);
+    text?.send(`\`${title}\` got stuck.`).catch(() => null);
+})
     .on("nodeConnect", ({ options }) => console.log(`${bot_1.shard} Lava ${options.host}:${options.port} connected.`))
     .on("nodeError", ({ options }, error) => console.log(`${bot_1.shard} Lava ${options.host}:${options.port} had an error: ${error.message}`))
     .on("trackStart", async ({ voiceChannel, textChannel, destroy }, track) => {
     const voice = client.channels.cache.get(voiceChannel);
     const text = client.channels.cache.get(textChannel);
-    if (!voice?.members.filter((m) => m.user.id != client.user.id).size)
-        return text?.send("Все участники покинули голосовой канал. Останавливаю плеер.").catch(() => null) && destroy();
+    if (!voice?.members.filter((m) => m.user.id != client.user.id).size) {
+        destroy();
+        return text?.send("Все участники покинули голосовой канал. Останавливаю плеер.").catch(() => null);
+    }
+    ;
     text?.send(`Играю:\n\`${track.title}\``).catch(() => null);
 })
     .on("queueEnd", async ({ textChannel, destroy }) => {
