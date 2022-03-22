6d60cf81d9800469809b43cd8f7e25f43db97e5a
itzJOHv <me@djoh.xyz>
2022-03-22 11:37:57 +0200

    Update lava.ts

--- src/handlers/lava.ts
+++ src/handlers/lava.ts
@@ -12,16 +12,26 @@ export = (client: ModifiedClient) => new Manager({
         client.guilds.cache.get(id)?.shard.send(payload);
     }
 })
-    .on("trackError", console.log)
-    .on("trackStuck", console.log)
+    .on("trackError", ({ textChannel }, { title }, error) => {
+        const text = client.channels.cache.get(textChannel) as TextChannel;
+
+        text?.send(`An error occured when trying to play \`${title}\`: ${error.exception?.cause || error.error}`).catch(() => null);
+    })
+    .on("trackStuck", ({ textChannel }, { title }, error) => {
+        const text = client.channels.cache.get(textChannel) as TextChannel;
+
+        text?.send(`\`${title}\` got stuck.`).catch(() => null);
+    })
     .on("nodeConnect", ({ options }) => console.log(`${shard} Lava ${options.host}:${options.port} connected.`))
     .on("nodeError", ({ options }, error) => console.log(`${shard} Lava ${options.host}:${options.port} had an error: ${error.message}`))
     .on("trackStart", async ({ voiceChannel, textChannel, destroy }, track) => {
         const voice = client.channels.cache.get(voiceChannel) as VoiceBasedChannel;
         const text = client.channels.cache.get(textChannel) as TextChannel;
 
-        if (!voice?.members.filter((m) => m.user.id != client.user.id).size)
-            return text?.send("Все участники покинули голосовой канал. Останавливаю плеер.").catch(() => null) && destroy();
+        if (!voice?.members.filter((m) => m.user.id != client.user.id).size) {
+            destroy();
+            return text?.send("Все участники покинули голосовой канал. Останавливаю плеер.").catch(() => null);
+        };
 
         text?.send(`Играю:\n\`${track.title}\``).catch(() => null);
     })
