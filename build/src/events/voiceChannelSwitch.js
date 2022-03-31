b0b966ea5e696abe7facb1ead9f1a9827b2300ac
itzJOHv <me@djoh.xyz>
2022-03-31 19:29:12 +0300

    .

--- build/src/events/voiceChannelSwitch.js
+++ build/src/events/voiceChannelSwitch.js
@@ -5,19 +5,24 @@ exports.name = "voiceChannelSwitch";
 async function run(client, member, oldChannel, newChannel) {
     const gset = await client.db.settings(member.guild.id);
     const { voices } = gset.get();
-    if (!voices.enabled || voices.lobby != oldChannel.id)
-        return;
     const gdb = await client.db.guild(member.guild.id);
-    member.guild.channels.create("Комната " + member.user.tag, {
-        type: "GUILD_VOICE",
-        parent: newChannel.parentId,
-        permissionOverwrites: [{
-                id: member.user.id,
-                allow: ["MANAGE_CHANNELS", "PRIORITY_SPEAKER", "STREAM", "CONNECT", "SPEAK"]
-            }]
-    }).then((ch) => {
-        member.voice.setChannel(ch.id).then(() => gdb.setOnObject("voices", ch.id, member.user.id)).catch(() => null);
-    }).catch(() => null);
+    if (gdb.get().voices[oldChannel.id] == member.user.id) {
+        oldChannel.delete().catch(() => null) && gdb.removeFromObject("voices", oldChannel.id);
+    }
+    ;
+    if (voices.lobby == newChannel.id && voices.enabled) {
+        member.guild.channels.create("Комната " + member.user.tag, {
+            type: "GUILD_VOICE",
+            parent: newChannel.parentId,
+            permissionOverwrites: [{
+                    id: member.user.id,
+                    allow: ["MANAGE_CHANNELS", "PRIORITY_SPEAKER", "STREAM"]
+                }]
+        }).then((ch) => {
+            member.voice.setChannel(ch.id).catch(() => null) && gdb.setOnObject("voices", ch.id, member.user.id);
+        }).catch(() => null);
+    }
+    ;
 }
 exports.run = run;
 ;
