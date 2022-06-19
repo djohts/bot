4e2c8faff2cc89ef7288d12192b81a1a841dcbb0
Alex <me@djoh.xyz>
2022-06-19 22:03:59 +0000

    add docs command

---
+++ build/src/commands/docs.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.run = exports.permission = exports.options = void 0;
+const builders_1 = require("@discordjs/builders");
+exports.options = new builders_1.SlashCommandBuilder()
+    .setName("docs")
+    .setDescription("Документация по использованию бота.")
+    .toJSON();
+exports.permission = 0;
+const run = async (interaction) => {
+    await interaction.reply({
+        content: "Документация: https://djoh.gitbook.io/djoho-bot",
+        ephemeral: true
+    });
+};
+exports.run = run;
---
+++ src/commands/docs.ts
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const options = new SlashCommandBuilder()
+    .setName("docs")
+    .setDescription("Документация по использованию бота.")
+    .toJSON();
+export const permission = 0;
+
+import { CommandInteraction } from "discord.js";
+
+export const run = async (interaction: CommandInteraction): Promise<any> => {
+    await interaction.reply({
+        content: "Документация: https://djoh.gitbook.io/djoho-bot",
+        ephemeral: true
+    })
+};
